065f543ea2d1f6c466a96d9cacac0f612cf96867
aswanthpp <ppaswanth3@gmail.com>
2018-06-03 22:25:02 +0530

    login page src added

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-var express =require('express');
-var path =  require('path');
-var bodyparser= require('body-parser');
+﻿require('rootpath')();
+var express = require('express');
+var app = express();
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var expressJwt = require('express-jwt');
+var config = require('config.json');
 
-var index = require('./routes/index');
-var tasks = require('./routes/tasks');
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: config.secret, resave: false, saveUninitialized: true }));
 
-var app= express();
+// use JWT auth to secure the api
+app.use('/api', expressJwt({ secret: config.secret }).unless({ path: ['/api/users/authenticate', '/api/users/register'] }));
 
-var port = 3000;
-//view Engine 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.engine('html',require('ejs').renderFile);
+// routes
+app.use('/login', require('./controllers/login.controller'));
+app.use('/register', require('./controllers/register.controller'));
+app.use('/app', require('./controllers/app.controller'));
+app.use('/api/users', require('./controllers/api/users.controller'));
 
+// make '/app' default route
+app.get('/', function (req, res) {
+    return res.redirect('/app');
+});
 
-//set Static folder
-app.use(express.static(path.join(__dirname,'client')));
-
-//Body Parser 
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({extended: false}));
-
-app.use('/',index);
-app.use('/api',tasks);
-
-app.listen(port, function(){
-	console.log('server started on port ' + port);
+// start server
+var server = app.listen(3000, function () {
+    console.log('Server listening at http://' + server.address().address + ':' + server.address().port);
 });
